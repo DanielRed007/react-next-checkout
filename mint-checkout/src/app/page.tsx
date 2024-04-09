3786fb46226ecb1d5ba85fea68c95eaa341573d3
danielred007 <danielrojo1927@gmail.com>
2024-04-09 21:52:24 +0100

    adding Radix UI setup

--- mint-checkout/src/app/page.tsx
+++ mint-checkout/src/app/page.tsx
@@ -1,6 +1,19 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 export default function Home() {
-  return <h1>Hello Next App!</h1>;
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.container}>
+        <div className={`${styles.item}`}>
+          Big Column (7/10)
+          <Flex direction="column" gap="2">
+            <Text>Hello from Radix Themes :)</Text>
+            <Button>Let's go</Button>
+          </Flex>
+        </div>
+        <div className={`${styles.item}`}>Small Column (3/10)</div>
+      </div>
+    </div>
+  );
 }

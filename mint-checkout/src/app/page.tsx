b6915f9df53b437f0af8117766291c1abd2e4fce
danielred007 <danielrojo1927@gmail.com>
2024-04-10 22:38:04 +0100

    Radix is tricky, i would rather go back to my beautiful Tailwind

--- mint-checkout/src/app/page.tsx
+++ mint-checkout/src/app/page.tsx
@@ -1,20 +1,41 @@
-import { PersonalDataForm } from "./components/PersonalDataForm";
+import { PersonalDataForm } from "./components/PersonalDataForm/PersonalDataForm";
 import styles from "./page.module.css";
-import { Text } from "@radix-ui/themes";
+import { Text, Box, Container, Grid } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <div className={styles.homeContainer}>
-      <div className={styles.container}>
-        <div className={`${styles.item}`}>
-          <div>
-            <Text size="9">Your Order</Text>
-          </div>
-
-          <PersonalDataForm />
-        </div>
-        <div className={`${styles.item}`}>Small Column (3/10)</div>
-      </div>
-    </div>
+    <Box>
+      <Container size="4">
+        <Grid
+          style={{
+            background: "var(--gray-a2)",
+          }}
+          columns={{ md: "2" }}
+          gap="3"
+          width="100%"
+        >
+          <Box height="64px">
+            <div
+              style={{
+                borderRadius: "var(--radius-3)",
+                width: "100%",
+                height: "100%",
+                backgroundColor: "blue",
+              }}
+            ></div>
+          </Box>
+          <Box height="64px">
+            <div
+              style={{
+                borderRadius: "var(--radius-3)",
+                width: "100%",
+                height: "100%",
+                backgroundColor: "blue",
+              }}
+            ></div>
+          </Box>
+        </Grid>
+      </Container>
+    </Box>
   );
 }

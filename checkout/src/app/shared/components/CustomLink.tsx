eb9b84dbacecd1c466d984a1de85ecdde3f17b00
danielred007 <danielrojo1927@gmail.com>
2024-05-20 21:42:30 +0100

    adding header refactor

--- checkout/src/app/shared/components/CustomLink.tsx
+++ checkout/src/app/shared/components/CustomLink.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import Link from "next/link";
-import { CustomRoute } from "../model/CustomRoute";
+import { CustomRoute } from "@/app/model/CustomRoute";
 
 interface Props {
   routes: CustomRoute[];
@@ -10,7 +10,7 @@ export const CustomLink: FC<Props> = ({ routes }) => {
   return (
     <ul className="flex items-center">
       {routes.map((route, index) => (
-        <li className="px-2">
+        <li key={index} className="px-2">
           <Link key={index} href={route.link}>
             {route.title}
           </Link>

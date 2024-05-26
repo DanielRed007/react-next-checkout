4f7ce1fb5d45aa7b1270953c942d22cfeaf831e4
danielred007 <danielrojo1927@gmail.com>
2024-05-26 20:15:09 +0100

    adding selector componet for cart

--- checkout/src/app/layout.tsx
+++ checkout/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,300;1,400&family=Quicksand:wght@300..700&display=swap"
           rel="stylesheet"
--- checkout/src/app/shared/components/CartModal.tsx
+++ checkout/src/app/shared/components/CartModal.tsx
@@ -1,8 +1,32 @@
+import { Fragment, useState } from "react";
 import { useHomeContext } from "@/app/context/HomeContext";
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { RadioGroup } from "@headlessui/react";
+import { CheckIcon } from "../icons/CheckIcon";
+
+const plans = [
+  {
+    name: "Startup",
+    ram: "12GB",
+    cpus: "6 CPUs",
+    disk: "160 GB SSD disk",
+  },
+  {
+    name: "Business",
+    ram: "16GB",
+    cpus: "8 CPUs",
+    disk: "512 GB SSD disk",
+  },
+  {
+    name: "Enterprise",
+    ram: "32GB",
+    cpus: "12 CPUs",
+    disk: "1024 GB SSD disk",
+  },
+];
 
 export const CartModal = () => {
+  const [selected, setSelected] = useState(plans[0]);
   const { isCartOpen, closeCart } = useHomeContext();
 
   return (
@@ -40,6 +64,75 @@ export const CartModal = () => {
                 </Dialog.Title>
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">Review Your Items</p>
+
+                  <div className="w-full px-4 py-16">
+                    <div className="mx-auto w-full max-w-md">
+                      <RadioGroup value={selected} onChange={setSelected}>
+                        <RadioGroup.Label className="sr-only">
+                          Server size
+                        </RadioGroup.Label>
+                        <div className="space-y-2">
+                          {plans.map((plan) => (
+                            <RadioGroup.Option
+                              key={plan.name}
+                              value={plan}
+                              className={({ active, checked }) =>
+                                `${
+                                  active
+                                    ? "ring-2 ring-white/60 ring-offset-2 ring-offset-sky-300"
+                                    : ""
+                                }
+                  ${checked ? "bg-sky-900/75 text-white" : "bg-white"}
+                    relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+                              }
+                            >
+                              {({ active, checked }) => (
+                                <>
+                                  <div className="flex w-full items-center justify-between">
+                                    <div className="flex items-center">
+                                      <div className="text-sm">
+                                        <RadioGroup.Label
+                                          as="p"
+                                          className={`font-medium  ${
+                                            checked
+                                              ? "text-white"
+                                              : "text-gray-900"
+                                          }`}
+                                        >
+                                          {plan.name}
+                                        </RadioGroup.Label>
+                                        <RadioGroup.Description
+                                          as="span"
+                                          className={`inline ${
+                                            checked
+                                              ? "text-sky-100"
+                                              : "text-gray-500"
+                                          }`}
+                                        >
+                                          <span>
+                                            {plan.ram}/{plan.cpus}
+                                          </span>{" "}
+                                          <span aria-hidden="true">
+                                            &middot;
+                                          </span>{" "}
+                                          <span>{plan.disk}</span>
+                                        </RadioGroup.Description>
+                                      </div>
+                                    </div>
+                                    {checked && (
+                                      <div className="shrink-0 text-white">
+                                        <CheckIcon />
+                                      </div>
+                                    )}
+                                  </div>
+                                </>
+                              )}
+                            </RadioGroup.Option>
+                          ))}
+                        </div>
+                      </RadioGroup>
+                    </div>
+                  </div>
                 </div>
 
                 <div className="mt-4">
